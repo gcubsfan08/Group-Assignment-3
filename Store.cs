fe37c705f7f4cd4369f5c180823ea096d60bf960
gcubsfan08 <93947261+gcubsfan08@users.noreply.github.com>
2021-11-09 21:12:22 -0600

    Create Store.cs

---
+++ Store.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_Assignment_3
+{
+    class Store
+    {
+        public string StoreName { get; set; }
+
+        public Store(string storeName)
+        {
+            StoreName = storeName;
+        }
+    }
+}
